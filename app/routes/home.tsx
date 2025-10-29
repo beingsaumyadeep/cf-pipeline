80043977ec014eb0e91e0e24b3791a6721b8d4bb
Sam <saumyadeeps1@gmail.com>
2025-10-30 02:11:02 +0530

    blow 1

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,17 @@
+import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta() {
+export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
-export default function Home() {
-  return <Welcome />;
+export function loader({ context }: Route.LoaderArgs) {
+  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return <Welcome message={loaderData.message} />;
 }
